2ca9f9d3d24b88eb8c27c6e29c5b84a0c8c01358
marcelopaschoarelii <marcelo.paschoareli23@gmail.com>
2025-08-22 07:20:07 -0300

    refactor(style): change branch name

--- app/src/main/java/com/example/eitruck/MainActivity.kt
+++ app/src/main/java/com/example/eitruck/MainActivity.kt
@@ -1,32 +1,20 @@
-package com.example.eitruck.ui
+package com.example.eitruck
 
-import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.eitruck.MainActivity
-import com.example.eitruck.R
 
-class SplashScreen : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_splash_screen)
+        setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-    }
-
-    fun redirecionarPagina(): Void{
-        Handler.createAsync(Looper.getMainLooper()).postDelayed({
-
-        },3000)
     }
 }
