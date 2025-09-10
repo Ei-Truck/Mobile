3fa0dd67636dc0212613084ef503c8f986131b77
marcelopaschoarelii <marcelo.paschoareli23@gmail.com>
2025-09-10 16:05:47 -0300

    feat(home): create graph and recycler view

---
+++ app/src/main/java/com/example/eitruck/ui/home/HomeAdapter.kt
@@ -0,0 +1,41 @@
+package com.example.eitruck.ui.home
+
+import MotoristaRanking
+import android.graphics.drawable.GradientDrawable
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.example.eitruck.R
+
+class HomeAdapter (
+    private val motoristas: List<MotoristaRanking>
+) : RecyclerView.Adapter<HomeViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomeViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.card_ranking, parent, false)
+        return HomeViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: HomeViewHolder, position: Int) {
+        val motorista = motoristas[position]
+        holder.nomeMotorista.text = motorista.nome
+        holder.pontuacao.text = motorista.pontuacao.toString()
+        holder.posicao.text = (position + 1).toString()
+
+        val drawable = holder.posicao.background.mutate()
+        if (drawable is GradientDrawable) {
+            val cor = ContextCompat.getColor(holder.itemView.context, motorista.cor)
+            drawable.setColor(cor)
+        }
+    }
+
+
+
+    override fun getItemCount(): Int {
+        return motoristas.size
+    }
+
+
+
+}
--- app/src/main/java/com/example/eitruck/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/eitruck/ui/home/HomeFragment.kt
@@ -1,16 +1,17 @@
 package com.example.eitruck.ui.home
 
+import MotoristaRanking
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.eitruck.R
 import com.github.mikephil.charting.charts.CombinedChart
-import com.github.mikephil.charting.components.XAxis
 import com.github.mikephil.charting.data.*
-import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
-import com.github.mikephil.charting.formatter.ValueFormatter
+
 
 class HomeFragment : Fragment() {
 
@@ -29,91 +30,48 @@ class HomeFragment : Fragment() {
         val valores = arrayOf(7f, 15f, 12f, 11f, 20f)
         val labels = arrayOf("Segunda", "Terça", "Quarta", "Quinta", "Sexta")
 
-        val barEntries = ArrayList<BarEntry>()
-        val lineEntries = ArrayList<Entry>()
+        HomeGraph(combinedChart, valores, labels, requireContext())
 
-        for (i in valores.indices) {
-            barEntries.add(BarEntry(i.toFloat(), valores[i]))
-            lineEntries.add(Entry(i.toFloat(), valores[i]))
+        val lista: List<MotoristaRanking> = listOf(
+            MotoristaRanking(1, "Motorista fuiweiovjewiojewiogj", 1000),
+            MotoristaRanking(2, "Motorista 2", 750),
+            MotoristaRanking(3, "Motorista 3", 500),
+            MotoristaRanking(4, "Motorista 4", 250),
+            MotoristaRanking(5, "Motorista 5", 100)
+            ,
+            MotoristaRanking(6, "Motorista 5", 100),
+            MotoristaRanking(7, "Motorista 5", 100),
+            MotoristaRanking(8, "Motorista 5", 100),
+            MotoristaRanking(9, "Motorista 5", 100),
+            MotoristaRanking(10, "Motorista 5", 100)
+        )
+
+        val recyclerView = view.findViewById<RecyclerView>(R.id.ranking)
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        recyclerView.adapter = HomeAdapter(lista)
+        recyclerView.isNestedScrollingEnabled = false
+
+        recyclerView.post {
+            recyclerView.expand()
         }
-
-        val barDataSet = BarDataSet(barEntries, "Vendas").apply {
-            setColors(
-                resources.getColor(R.color.colorPrimary),
-                resources.getColor(R.color.colorSecondary),
-                resources.getColor(R.color.colorTertiary),
-                resources.getColor(R.color.colorPrimary),
-                resources.getColor(R.color.colorPrimaryDark)
-            )
-            valueTextSize = 16f
-            valueFormatter = object : ValueFormatter() {
-                override fun getBarLabel(barEntry: BarEntry?): String {
-                    return barEntry?.y?.toInt().toString()
-                }
-            }
-        }
-
-        val lineDataSet = LineDataSet(lineEntries, "Tendência").apply {
-            color = resources.getColor(R.color.textColorSecondary)
-            lineWidth = 2f
-
-            setDrawCircles(true)
-            circleRadius = 5f
-            valueTextSize = 14f
-            valueFormatter = object : ValueFormatter() {
-                override fun getPointLabel(entry: Entry?): String {
-                    return entry?.y?.toInt().toString()
-                }
-            }
-
-            setCircleColor(resources.getColor(R.color.textColorSecondary))
-
-            setDrawValues(false)
-        }
-
-        val combinedData = CombinedData().apply {
-            setData(BarData(barDataSet))
-            setData(LineData(lineDataSet))
-        }
-
-
-        combinedChart.data = combinedData
-
-        val xAxis = combinedChart.xAxis
-        xAxis.position = XAxis.XAxisPosition.BOTTOM
-        xAxis.granularity = 1f
-        xAxis.valueFormatter = IndexAxisValueFormatter(labels)
-        xAxis.textSize = 16f
-        xAxis.typeface = resources.getFont(R.font.texto_inter_regular)
-        xAxis.setDrawGridLines(false)
-        xAxis.setDrawAxisLine(false)
-        xAxis.axisMinimum = -0.5f
-        xAxis.axisMaximum = valores.size - 0.5f
-
-
-        combinedChart.axisLeft.isEnabled = false
-        combinedChart.axisRight.isEnabled = false
-        combinedChart.setDrawGridBackground(false)
-        combinedChart.setDrawBorders(false)
-        combinedChart.setScaleEnabled(false)
-        combinedChart.setBackgroundColor(resources.getColor(R.color.white))
-        combinedChart.setExtraBottomOffset(15f)
-
-        val barData = BarData(barDataSet)
-        barData.barWidth = 0.8f
-        combinedData.setData(barData)
-
-
-
-        combinedChart.legend.isEnabled = false
-        combinedChart.description.isEnabled = false
-
-        barDataSet.setDrawValues(false)
-        lineDataSet.setDrawValues(true)
-
-        combinedChart.setDescription(combinedChart.description)
-
-
-        combinedChart.invalidate()
     }
+
+
+    // Esse código é para expandir o recycler view
+    fun RecyclerView.expand() {
+        val adapter = adapter ?: return
+        var totalHeight = 0
+        for (i in 0 until adapter.itemCount) {
+            val holder = adapter.createViewHolder(this, adapter.getItemViewType(i))
+            adapter.onBindViewHolder(holder, i)
+            holder.itemView.measure(
+                View.MeasureSpec.makeMeasureSpec(width, View.MeasureSpec.EXACTLY),
+                View.MeasureSpec.UNSPECIFIED
+            )
+            totalHeight += holder.itemView.measuredHeight
+        }
+        layoutParams.height = totalHeight
+        requestLayout()
+    }
+
 }
